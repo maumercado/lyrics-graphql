54b61531aa4936cc16788b27dfbb61da757361db
Mauricio Mercado <mau@maumercado.com>
2017-12-03 23:39:25 -0800

    Added a dispatch of created song just to track it in the state of the app

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import { CREATED_NEW_SONG } from "./types";
 
 export const submitSongCreate = title => dispatch => {
-    return { type: CREATED_NEW_SONG, payload: title };
+    return dispatch({ type: CREATED_NEW_SONG, payload: title });
 };
