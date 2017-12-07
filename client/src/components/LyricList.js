626d3bd45008fa6acd7244b34920ee0b025b0339
Mauricio Mercado <mau@maumercado.com>
2017-12-06 22:59:21 -0800

    Add optimistic updates with apollo
    Add likes to fetchsong query
    
    Fixed some styles

--- client/src/components/LyricList.js
+++ client/src/components/LyricList.js
@@ -1,20 +1,35 @@
 import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 
 class LyricList extends Component {
-    onLike = id => {
-        console.log(id);
+    onLike = (id, likes) => {
+        this.props.mutate({
+            variables: { id },
+            optimisticResponse: {
+                __typename: "Mutation",
+                likeLyric: {
+                    id: id,
+                    __typename: "LyricType",
+                    likes: likes + 1
+                }
+            }
+        });
     };
     renderLyrics = () => {
-        return this.props.lyrics.map(({ id, content }) => {
+        return this.props.lyrics.map(({ id, content, likes }) => {
             return (
                 <li key={id} className="collection-item">
                     {content}
-                    <i
-                        className="material-icons"
-                        onClick={() => this.onLike(id)}
-                    >
-                        thumb_up
-                    </i>
+                    <div className="vote-box">
+                        <i
+                            className="material-icons right"
+                            onClick={() => this.onLike(id, likes)}
+                        >
+                            thumb_up
+                        </i>
+                        {likes}
+                    </div>
                 </li>
             );
         });
@@ -25,4 +40,13 @@ class LyricList extends Component {
     }
 }
 
-export default LyricList;
+const mutation = gql`
+    mutation LikeLyric($id: ID!) {
+        likeLyric(id: $id) {
+            id
+            likes
+        }
+    }
+`;
+
+export default graphql(mutation)(LyricList);
