48a7e758c3617b103bb3b4315b08c5d3be105c3d
Mauricio Mercado <mau@maumercado.com>
2017-12-06 22:22:29 -0800

    Add a mapping to cache for using the ids of the objects to store and relate every object in the store of apollo graphql

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -2,7 +2,9 @@ import { ApolloClient, HttpLink, InMemoryCache } from "apollo-client-preset";
 
 const client = new ApolloClient({
     link: new HttpLink(),
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({
+        dataIdFromObject: o => o.id
+    }),
     connectToDevTools: process.env.NODE_ENV !== "production"
 });
 
